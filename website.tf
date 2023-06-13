670b999309999cc3e906ef4d5cd51eb2165fa718
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2023-06-13 16:00:51 +0530

    [MODIFIED] script for enablic Static website hosting in S3

--- website.tf
+++ website.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket_website_configuration" "tf_webiste" {
+resource "aws_s3_bucket_website_configuration" "tf_website" {
   bucket = aws_s3_bucket.tf_bucket.id
 
   index_document {
